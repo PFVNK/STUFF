2796aaca3ea0fdd38b88dc001efa55240850b2c4
Patrick Heflin <patrickslx@yahoo.com>
2019-04-11 22:21:16 -0500

    changed props being mapped

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import StackGrid from 'react-stack-grid'
 import Loader from 'react-loader-spinner'
+import LazyLoad from 'react-lazyload';
+
 
 import '../App.css';
 
@@ -23,31 +25,34 @@ class Gallery extends Component {
 
     render() {
         return (
-            <div className='gallery-main'>
-                {this.state.isLoading && this.props.tagResults !== true ?
-                    <Loader
-                        style={'gallery-spinner'}
-                        type="Grid"
-                        color="lightgreen"
-                        height="200"
-                        width="200"
-                    />
-                    :
-
-                    <StackGrid
-                        className='gallery-grid'
-                        columnWidth={330}
-                        monitorImagesLoaded={true}
-                        gutterHeight={9}
-                        gutterWidth={7}>
-                        {this.props.tagResults.map((x, index) => <div key={index} className='gallery-item-wrap'><div key={index} className='gallery-item'>
-                            <a href={x.url} target='_blank' rel="noopener noreferrer"><img className='gallery-img' src={x.images} alt={x.title} /></a>
-                            <a href={x.url} target='_blank' rel="noopener noreferrer"><h2>{x.title}</h2></a>
-                            <h3>PRICE: {x.price}</h3>
-                            <p>LOCATION: {x.hood}</p>
-                        </div></div>)}
-                    </StackGrid>}
-            </div>
+            <>
+                <div className='gallery-main'>
+                    {this.state.isLoading && this.props.tagResults !== true ?
+                        <Loader
+                            style={'gallery-spinner'}
+                            type="Grid"
+                            color="lightgreen"
+                            height="200"
+                            width="200"
+                        />
+                        :
+                        
+                        <StackGrid
+                            className='gallery-grid'
+                            columnWidth={330}
+                            monitorImagesLoaded={true}
+                            gutterHeight={9}
+                            gutterWidth={7}>
+                            {this.props.items.map((x, index) => <div key={index} className='gallery-item-wrap'><div key={index} className='gallery-item'>
+                                <a href={x.url} target='_blank' rel="noopener noreferrer"><img className='gallery-img' src={x.images} alt={x.title} /></a>
+                                <a href={x.url} target='_blank' rel="noopener noreferrer"><h2>{x.title}</h2></a>
+                                <h3>PRICE: {x.price}</h3>
+                                <p>LOCATION: {x.hood}</p>
+                            </div></div>)}
+                        </StackGrid>}
+                    <button className='gallery-button' onClick={this.props.lazyLoad}>Show More <span className='gallery-stuff-text'>STUFF</span></button>
+                </div>
+            </>
         )
     }
 }
