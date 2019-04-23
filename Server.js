f20294ea6cc2c2831c84959435af7bfc13813c7e
Patrick Heflin <patrickslx@yahoo.com>
2019-04-23 14:05:56 -0500

    update

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
+const path = require('path')
 
 const app = express()
 
@@ -103,4 +104,15 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(3001, () => { console.log('listening on port 3001...') })
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
