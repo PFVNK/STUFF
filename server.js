8e88f6246ac592e68c328ba4bcd18ccd5228c78e
Patrick Heflin <patrickslx@yahoo.com>
2019-05-03 07:46:41 -0500

    change file paths

--- server.js
+++ server.js
@@ -106,10 +106,10 @@ app.use((err, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use('/static', express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
 
