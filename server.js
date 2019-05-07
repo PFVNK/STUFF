ec8254cfb83b8b2563a21b81eecbdce031aa9bde
Patrick Heflin <patrickslx@yahoo.com>
2019-05-07 08:51:04 -0500

    *

--- server.js
+++ server.js
@@ -97,9 +97,13 @@ if (process.env.NODE_ENV === 'production') {
   //Set static folder
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+})
 
 const port = process.env.PORT || 3001;
 
